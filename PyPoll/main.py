f32faf951f3fcbed29d7a084952af356dce36b46
jerryevalentine <134810823+jerryevalentine@users.noreply.github.com>
2024-02-25 14:38:48 -0800

    Add files via upload

---
+++ PyPoll/main.py
@@ -0,0 +1,38 @@
+#Help a small, rural town modernize its vote-counting process.
+
+"""
+Poll data called election_data.csv. 
+The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
+Python script that analyzes the votes and calculates each of the following values:
+
+    The total number of votes cast
+    A complete list of candidates who received votes
+    The percentage of votes each candidate won
+    The total number of votes each candidate won
+    The winner of the election based on popular vote
+    
+Your analysis should align with the following results:
+
+Election Results
+-------------------------
+Total Votes: 369711
+-------------------------
+Charles Casper Stockham: 23.049% (85213)
+Diana DeGette: 73.812% (272892)
+Raymon Anthony Doane: 3.139% (11606)
+-------------------------
+Winner: Diana DeGette
+-------------------------
+
+		
+Row Labels	Count of County	
+Charles Casper Stockham	440	
+Diana DeGette	733	
+Raymon Anthony Doane	44	
+Grand Total	1217	
+		
+
+
+
+"""
+
